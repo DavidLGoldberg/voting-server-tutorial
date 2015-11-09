871841056c36dbc38dabeea7fe6c2a28bb165aaf
David L. Goldberg <davidlgoldberg@gmail.com>
2015-11-09 12:47:20 -0500

    Add log both states log if new property added

--- src/middleware/scream.js
+++ src/middleware/scream.js
@@ -1,14 +1,24 @@
 import { isFSA } from 'flux-standard-action';
 
-export default function screamMiddleware({ dispatch }) {
-  return next => action => {
-    if (!isFSA(action)) {
-      return next(action);
-    }
+const screamer = store => next => action => {
+  console.log('^^^^^^^^^^^');
+  console.log(action);
+  let state1 = store.getState();
+  let result = next(action);
+  console.log(result);
+  let state2 = store.getState();
+  if (state2.greeter.includes('David') && !state1.greeter.includes('David')) {
+    console.log('added David!!!!!!!!!!!!');
+  }
+  console.log('state1', state1);
+  console.log('state2', state2);
+  console.log('$$$$$$$$$$$');
+  return action;
+};
 
-    console.log('^^^^^^^^^^^');
-    console.log(action);
-    console.log('$$$$$$$$$$$');
-    return next(action);
-  };
-}
+// export default function screamMiddleware({ dispatch }) {
+//   return store => next => action => {
+//   };
+// }
+
+export default screamer;
